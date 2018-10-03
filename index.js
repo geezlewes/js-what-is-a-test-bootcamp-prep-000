d1789d97c56bdca17ea6252b396e0b00ea9e8b72
Luis Rafael Santiago Villalta <santiagovillalta@outlook.com>
2018-10-03 14:44:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-describe ('Name'), ()=>{
+describe ('Name', ()=>{
   it('returns "Susan"', () =>{
     expect(index.name).toEqual('Susan')
   })
 })
-describe("Height"), () => {
+describe("Height", () => {
 it ("is less than 40",()=> {
   expect (index.height).tobeLessThan(40)
 })
 })
-describe ("Message")=> {
+describe ("Message",()=> {
   it("gives the name and height",() => {
     expect(index.message).toInclude(index.name)
     expect(index.message).toInclude(index.height)
